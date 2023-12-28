7d42e25329e3e2ff959f268b592ba2676359e399
xixisherry <136421219+xixisherry@users.noreply.github.com>
2023-12-28 18:05:44 +0800

    Update 2. 24小时取余数.cpp

--- 2. 24小时取余数.cpp
+++ 2. 24小时取余数.cpp
@@ -1,3 +1,16 @@
+// 给你一个正整数 arrivalTime 表示列车正点到站的时间（单位：小时），另给你一个正整数 delayedTime 表示列车延误的小时数。
+
+// 返回列车实际到站的时间。
+
+// 注意，该问题中的时间采用 24 小时制.
+
+// 示例 1：
+
+// 输入：arrivalTime = 15, delayedTime = 5 
+// 输出：20 
+// 解释：列车正点到站时间是 15:00 ，延误 5 小时，所以列车实际到站的时间是 15 + 5 = 20（20:00）。
+
+//第一边做的时候没反应过来，实际上只需要return（arrivalTime+delayedTime）% 24;一行
 class Solution {
 public:
     int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
@@ -15,3 +28,5 @@ public:
         }
     }
 };
+
+//return（arrivalTime+delayedTime）% 24;
